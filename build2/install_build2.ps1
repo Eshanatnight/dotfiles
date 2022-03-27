119d679fe84d71771dbc8fd2c57674dade5ddb6e
Eshan Chatterjee <eshan2001221@gmail.com>
2022-03-27 16:52:21 +0530

    update install script

--- build2/install_build2.ps1
+++ build2/install_build2.ps1
@@ -1,7 +1,5 @@
-cd d:;
-mkdir build2-build2;
-cd build2-build2;
-mkdir build2-MSVC;
-cd build2-MSVC;
-certutil -hashfile build2-install-msvc-0.14.0.bat SHA256
-.\build2-install-msvc-0.14.0.bat
+mkdir "D:\build2-build2\build2-MSVC";
+Copy-Item .\build2-install-msvc-0.14.0.bat "D:\build2-build2\build2-MSVC";
+Set-Location D:\build2-build2\build2-MSVC;
+certutil -hashfile build2-install-msvc-0.14.0.bat SHA256;
+.\build2-install-msvc-0.14.0.bat;
