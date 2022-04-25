525079ac6af3121b22da81d47ab932c1cf7f8426
Eshan Chatterjee <eshan2001221@gmail.com>
2022-04-25 14:06:12 +0530

    added sqlite commands cheatsheet

---
+++ sqlite/sqlite_scripts.sh
@@ -0,0 +1,8 @@
+.read <file_name>    ->> Runs a sql script
+
+.mode column         ->> Displays the data in columns.. column formatting
+
+.headers on          ->> Displays the column names
+
+.shell cls           ->> Clears the screen
+
