2ad38d22550e08ded641513c219814d2df966106
Eshan Chatterjee <eshan2001221@gmail.com>
2022-08-23 01:07:55 +0530

    moved init-codeserver script to scripts dir

--- vscode server/init-codeserver.ps1
+++
@@ -1,3 +0,0 @@
-New-Item "$HOME\.vscode-server-launcher\bin" -ItemType "directory" -Force
-Invoke-WebRequest "https://aka.ms/vscode-server-launcher/aarch64-pc-windows-msvc" -OutFile "$HOME\.vscode-server-launcher\bin\code-server.exe"
-[Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", "User") + ";$HOME\.vscode-server-launcher\bin", "User")
