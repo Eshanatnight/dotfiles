ca08fc662bee80cc9331348af5165f63a2c9972e
Eshan Chatterjee <eshan2001221@gmail.com>
2022-08-28 21:33:01 +0530

    Fix improper download

--- scripts/init-codeserver.ps1
+++ scripts/init-codeserver.ps1
@@ -1,3 +1,3 @@
 New-Item "$HOME\.vscode-server-launcher\bin" -ItemType "directory" -Force
-Invoke-WebRequest "https://aka.ms/vscode-server-launcher/aarch64-pc-windows-msvc" -OutFile "$HOME\.vscode-server-launcher\bin\code-server.exe"
+Invoke-WebRequest "https://aka.ms/vscode-server-launcher/x86_64-pc-windows-msvc" -OutFile "$HOME\.vscode-server-launcher\bin\code-server.exe"
 [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", "User") + ";$HOME\.vscode-server-launcher\bin", "User")
