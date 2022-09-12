dea68f80afd45387c6e64e392beeaf7cf6863fa4
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-12 09:10:50 +0530

    Added tar scriplts

---
+++ scripts/ta.ps1
@@ -0,0 +1,35 @@
+$argsCount = $args.Count;
+
+$useage = "tc outfilename /path/to directory"
+
+if($argsCount -eq 0)
+{
+    Write-Error "Error Invalid Useage";
+    Write-Output "Useage:";
+    Write-Output $useage;
+    exit;
+}
+
+elseif($argsCount -gt 0 -or $argsCount -lt 3)
+{
+    $filename = $args[0] + ".tar.gz";
+    $path = $args[1];
+    $parms = "-czvf " + $filename + " ";
+
+    if(Test-Path $path)
+    {
+        $arr = $path.split('\');
+
+        $dirName = $arr[1];
+        $parms = $parms + $dirName;
+        Start-Process tar -ArgumentList $parms -NoNewWindow -Wait
+    }
+    else
+    {
+        Write-Error "Error Invalid Useage";
+        Write-Output "Useage:";
+        Write-Output $useage;
+        exit;
+    }
+}
+
---
+++ scripts/tu.ps1
@@ -0,0 +1,30 @@
+$argsCount = $args.Count;
+
+$useage = "tc /path/to/archive"
+
+if($argsCount -eq 0)
+{
+    Write-Error "Error Invalid Useage";
+    Write-Output "Useage:";
+    Write-Output $useage;
+    exit;
+}
+
+elseif($argsCount -eq 1)
+{
+    $path = $args[0];
+    $parms = "-xvf " + $path;
+
+    if(Test-Path $args[0])
+    {
+        Start-Process tar -ArgumentList $parms -NoNewWindow -Wait
+    }
+    else
+    {
+        Write-Error "Error Invalid Useage";
+        Write-Output "Useage:";
+        Write-Output $useage;
+        exit;
+    }
+}
+
