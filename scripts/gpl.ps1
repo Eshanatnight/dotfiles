f78d875ac0d9a48617c80c3239152b8906fba873
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-12 09:18:08 +0530

    made gpl easier

--- scripts/gpl.ps1
+++ scripts/gpl.ps1
@@ -1,5 +1,7 @@
+$url = "https://gist.githubusercontent.com/Eshanatnight/506ee5fd07aef19f26b4cdebd987979b/raw/16c96b2a56aab72e0316ddbd2371801346e1e33a/premake5.lua"
+
 if(!(Test-Path .\premake5.lua))
 {
     New-Item .\premake5.lua -ItemType File -Force
 }
-Invoke-WebRequest "https://gist.githubusercontent.com/Eshanatnight/506ee5fd07aef19f26b4cdebd987979b/raw/ef4f08eb12eec87dc48c999e02daa90feb79b4f8/premake5.lua" -OutFile "premake5.lua"
+Invoke-WebRequest $url -OutFile "premake5.lua"
