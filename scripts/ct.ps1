2cb6a9dbfde7398b5916c02e2dd7880dad45a859
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-16 20:26:01 +0530

    added clang-tidy script

---
+++ scripts/ct.ps1
@@ -0,0 +1,15 @@
+$dir = Get-Location;
+
+Get-ChildItem -Path . -Directory -Recurse |
+    foreach {
+        Set-Location $_.FullName
+        Start-Process clang-tidy -ArgumentList "*.cpp" -NoNewWindow -Wait
+    };
+
+Get-ChildItem -Path . -Directory -Recurse |
+    foreach {
+        Set-Location $_.FullName
+        Start-Process clang-tidy -ArgumentList "*.h" -NoNewWindow -Wait
+    };
+
+Set-Location $dir;
