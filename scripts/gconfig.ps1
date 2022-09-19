80f52e5bbdc0d1693f1000ce1c378c9dd23347c2
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-19 15:17:09 +0530

    Added Git All Config Script

---
+++ scripts/gconfig.ps1
@@ -0,0 +1,13 @@
+$dir = Get-Location;
+
+$i = 1;
+Get-ChildItem -Path . -Directory -Recurse -Depth 1 |
+    ForEach-Object -Process {
+        Set-Location $_.FullName
+        $current = Get-Location;
+        Write-Output "$i. Configuring $current..."
+        $i = $i + 1;
+        Start-Process git -ArgumentList "config --global --add safe.directory `"$current`"" -NoNewWindow -Wait -RedirectStandardError $dir/err.txt -RedirectStandardOutput $dir/out.txt
+}
+
+Set-Location $dir;
