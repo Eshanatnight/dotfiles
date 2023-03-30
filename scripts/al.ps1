4890f7a5700f83dba039a170741230d7be392c00
Eshan <eshan2001221@gmail.com>
2023-03-30 08:39:05 +0000

    Ammended al.ps1

--- scripts/al.ps1
+++ scripts/al.ps1
@@ -4,14 +4,19 @@ if ($argsLen -gt 1)
 {
     Write-Error "Invalid Useage"
     Write-Error "$useage"
+    exit
 }
 
-if($argsLen -ge 0)
-{
-    if(!(Test-Path .\LICENSE))
+$isRust = Test-Path .\Cargo.toml 
+$isCXX = Test-Path .\CmakeLists.txt -or Test-Path ".\*.sln"
+
+# General files
+function getLicense {
+        if(!(Test-Path .\LICENSE))
     {
-    New-Item .\LICENSE -ItemType File -Force
+        New-Item .\LICENSE -ItemType File -Force
     }
+
     Invoke-WebRequest "https://raw.githubusercontent.com/Eshanatnight/dotfiles/main/LICENSE" -OutFile ".\LICENSE"
 
     if(!(Test-Path .\LICENSE))
@@ -19,9 +24,10 @@ if($argsLen -ge 0)
         Write-Error "Failed to Create LICENSE file";
         exit;
     }
+    
+}
 
-    #-----------------------------------------------------------------------------#
-
+function getGitFiles {
     if(!(Test-Path .\.gitignore))
     {
         New-Item .\.gitignore -ItemType File -Force
@@ -34,8 +40,6 @@ if($argsLen -ge 0)
         Write-Error "Failed to Create .gitignore file";
         exit;
     }
-
-    #-----------------------------------------------------------------------------#
 
     if(!(Test-Path .\.gitattributes))
     {
@@ -51,57 +55,41 @@ if($argsLen -ge 0)
     }
 }
 
-if($argsLen -eq 1)
-{
-    if(!(Test-Path .\.clang-format))
-    {
-        New-Item .\.clang-format -ItemType File -Force
+getLicense()
+getGitFiles()
+
+if($isRust) {
+    # if Cargo.toml exists download the rustfmt file
+    # if the file already exists just overwrite it
+    New-Item .\rustfmt.toml -ItemType File -Force
+    Invoke-WebRequest "https://gist.githubusercontent.com/Eshanatnight/92e775458ce47910d908b8e80d9e0d2f/raw/eede8a414cde20a343b706ae34aef5106540da9e/rustfmt.toml" -OutFile ".\rustfmt.toml";
+
+    if(!(Test-Path .\rustfmt.toml)) {
+        Write-Error "Failed to Create rustfmt.toml file";
+        exit;
     }
+}
 
-    Invoke-WebRequest "https://gist.githubusercontent.com/Eshanatnight/ca162e562c7a0a53aebcc758b907f5f0/raw/72da840ce4bff61044e310af2c3b90f7f59d993a/.clang-format" -OutFile ".\.clang-format"
-
-    if(!(Test-Path .\.clang-format))
-    {
+if($isCXX) {
+    Invoke-WebRequest "https://gist.githubusercontent.com/Eshanatnight/ca162e562c7a0a53aebcc758b907f5f0/raw/72da840ce4bff61044e310af2c3b90f7f59d993a/.clang-format" -OutFile ".\.clang-format";
+    if(!(Test-Path .\.clang-format)){
         Write-Error "Failed to Create .clang-format file";
         exit;
     }
 
-    #-------------------------------------------------------------------------#
-
-    if(!(Test-Path .\vcpkg.json))
-    {
-        New-Item .\vcpkg.json -ItemType File -Force
-    }
-    Invoke-WebRequest "https://gist.githubusercontent.com/Eshanatnight/04e2babf3a774b21a12896ada50836b3/raw/725527ca384728fb4ac00d1809b81ad4cdb9b322/vcpkg.json" -OutFile "vcpkg.json"
-
-    if(!(Test-Path .\vcpkg.json))
-    {
+    Invoke-WebRequest "https://gist.githubusercontent.com/Eshanatnight/04e2babf3a774b21a12896ada50836b3/raw/725527ca384728fb4ac00d1809b81ad4cdb9b322/vcpkg.json" -OutFile "vcpkg.json";
+    if(!(Test-Path .\vcpkg.json)) {
         Write-Error "Failed to Create vcpkg.json file";
         exit;
     }
 
-    #-------------------------------------------------------------------------#
-
-    if(!(Test-Path .\premake5.lua))
-    {
-        New-Item .\premake5.lua -ItemType File -Force
-    }
-    Invoke-WebRequest "https://gist.githubusercontent.com/Eshanatnight/506ee5fd07aef19f26b4cdebd987979b/raw/16c96b2a56aab72e0316ddbd2371801346e1e33a/premake5.lua" -OutFile "premake5.lua"
-
-    if(!(Test-Path .\premake5.lua))
-    {
+    Invoke-WebRequest "https://gist.githubusercontent.com/Eshanatnight/506ee5fd07aef19f26b4cdebd987979b/raw/16c96b2a56aab72e0316ddbd2371801346e1e33a/premake5.lua" -OutFile "premake5.lua";
+    if(!(Test-Path .\premake5.lua)) {
         Write-Error "Failed to Create premake5.lua file";
         exit;
     }
 
-    #-------------------------------------------------------------------------#
-
-    if(!(Test-Path .\.clang-tidy))
-    {
-        New-Item .\.clang-tidy -ItemType File -Force
-    }
-    Invoke-WebRequest "https://gist.githubusercontent.com/Eshanatnight/2ef501048a515c5231fbc795a5369ed2/raw/19fcec1e007f28dbadc6cdb1bde076c3c087d8b2/.clang-tidy" -OutFile ".clang-tidy"
-
+    Invoke-WebRequest "https://gist.githubusercontent.com/Eshanatnight/2ef501048a515c5231fbc795a5369ed2/raw/19fcec1e007f28dbadc6cdb1bde076c3c087d8b2/.clang-tidy" -OutFile ".clang-tidy";
     if(!(Test-Path .\.clang-tidy))
     {
         Write-Error "Failed to Create .clang-tidy file";
