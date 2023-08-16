1c5b01f07e2930ba637e658e75a6658a0b915133
Eshan Chatterjee <eshan2001221@gmail.com>
2023-08-17 02:10:21 +0530

    fixed script bug and moved settings

--- install-homebrew.sh
+++ install-homebrew.sh
@@ -1,6 +1,6 @@
 cd ~;
-/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 test -d ~/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-test -r ~/.zprofile && echo "eval \"$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zprofile
+test -r ~/.profile && echo "eval \"$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
 echo "eval \"$($(brew --prefix")/bin/brew shellenv)\"" >> ~/.profile
