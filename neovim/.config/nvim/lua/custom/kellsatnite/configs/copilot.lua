74d6e565b25cd90c5485dcc1244ea19baa3e7f14
Eshan Chatterjee <eshan2001221@gmail.com>
2024-01-29 00:55:40 +0530

    copilot keymap update for accepting single word

--- neovim/.config/nvim/lua/custom/kellsatnite/configs/copilot.lua
+++ neovim/.config/nvim/lua/custom/kellsatnite/configs/copilot.lua
@@ -1,43 +1,43 @@
 local options = {
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<M-CR>",
-      refresh = "gr",
-      open = "<CR>"
+    panel = {
+        enabled = true,
+        auto_refresh = false, -- if set to true, panel will be refreshed as as you type
+        keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<M-CR>",
+            refresh = "gr",
+            open = "<CR>",
+        },
+        layout = {
+            position = "bottom", -- | top | left | right
+            ratio = 0.4,
+        },
     },
-    layout = {
-      position = "bottom", -- | top | left | right
-      ratio = 0.4
+    suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+            accept = "<M-l>",
+            accept_word = "<M-.>", -- to deactive false
+            accept_line = "<C-j>",
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+        },
     },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = true,
-    debounce = 75,
-    keymap = {
-      accept = "<M-l>",
-      accept_word = false,
-      accept_line = "<C-j>",
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
+    filetypes = {
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
     },
-  },
-  filetypes = {
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
-  },
-  copilot_node_command = 'node', -- Node.js version must be > 16.x
-  server_opts_overrides = {},
+    copilot_node_command = "node", -- Node.js version must be > 16.x
+    server_opts_overrides = {},
 }
 
 return options
