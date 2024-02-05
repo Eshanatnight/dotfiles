49733402ecc8d88b624f14524757d3e046f9de9b
Eshan Chatterjee <eshan2001221@gmail.com>
2024-02-05 20:01:26 +0530

    add protobuf lsp in plugins list

--- .config/nvim/lua/custom/kellsatnite/plugins.lua
+++ .config/nvim/lua/custom/kellsatnite/plugins.lua
@@ -30,6 +30,7 @@ local plugins = {
                 "typescript-language-server",
                 "bash-language-server",
                 "lua-language-server",
+                "protolint",
             },
         },
     },
