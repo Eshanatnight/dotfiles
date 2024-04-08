be116d97cc707df0f713921fb50e5864bfc063dd
Eshan Chatterjee <eshan2001221@gmail.com>
2024-04-08 22:14:21 +0530

    Remove unused PowerShell scripts

--- scripts/pwsh/android-cmd-line-tools.ps1
+++
@@ -1,4 +0,0 @@
-choco install flutter
-.\sdkmanager.bat "build-tools;30.0.0" --sdk_root=../
-.\sdkmanager.bat "platforms;android-30" --sdk_root=../
-.\sdkmanager.bat "cmdline-tools;latest" --sdk_root=../
