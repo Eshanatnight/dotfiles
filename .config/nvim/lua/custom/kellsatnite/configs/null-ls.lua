c7b9796c3dbb0133601ee2bf0f8d91510f786310
Eshan Chatterjee <eshan2001221@gmail.com>
2024-04-12 22:47:47 +0530

    update nonels

--- .config/nvim/lua/custom/kellsatnite/configs/null-ls.lua
+++ .config/nvim/lua/custom/kellsatnite/configs/null-ls.lua
@@ -3,12 +3,18 @@ local null_ls = require "null-ls"
 
 local opts = {
     sources = {
+        -- formatter sources
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports_reviser,
         null_ls.builtins.formatting.golines,
         null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.formatting.clang_format,
         null_ls.builtins.formatting.prettier,
+
+        -- diagnostics sources
+        null_ls.builtins.diagnostics.mypy,
+
+        -- breaks conform, will update it eventually
+        -- null_ls.builtins.formatting.clang_format,
     },
     on_attach = function(client, bufnr)
         if client.supports_method "textDocument/formatting" then
