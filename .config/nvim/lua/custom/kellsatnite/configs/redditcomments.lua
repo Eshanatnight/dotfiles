788dfee1f96cf9757a9f8fa96a3b0634ad8ad44c
Eshan Chatterjee <eshan2001221@gmail.com>
2024-04-27 23:31:02 +0530

    clean up redditcomments

--- .config/nvim/lua/custom/kellsatnite/configs/redditcomments.lua
+++ .config/nvim/lua/custom/kellsatnite/configs/redditcomments.lua
@@ -9,9 +9,9 @@ local options = {
             alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
             -- signs = false, -- configure signs for some keywords individually
         },
-        TODO = { icon = " ", color = "info", alt = { "?" } },
+        TODO = { icon = " ", color = "info" },
         HACK = { icon = " ", color = "warning" },
-        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX", "!" } },
         PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
         NOTE = { icon = " ", color = "hint", alt = { "INFO", "?" } },
         -- TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
