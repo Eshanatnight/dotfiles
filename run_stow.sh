24c9d0a4eb841b25e104d7ccc265786d9b5370ce
Eshan Chatterjee <eshan2001221@gmail.com>
2024-04-30 00:34:35 +0530

    update run stow script with example
    if i ever forget to run how to stow

--- run_stow.sh
+++ run_stow.sh
@@ -6,6 +6,9 @@ name=$(uname)
 if [[ $name == "Linux" ]]; then
     stow -d . -t /home/kellsatnite/ .
 else
-   echo "message" 
+   echo "message"
     stow -d . -t /Users/kellsatnite/ .
 fi
+
+# to stow a perticular dir
+# stow -d <folder_path> -t <target_folder_path> things to stow from dir
