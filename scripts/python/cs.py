017d394ffb68a7ec81465fb7ec590efc5d47889b
Eshan Chatterjee <eshan2001221@gmail.com>
2024-05-17 12:03:02 +0530

    cs.py has its own data src

--- scripts/python/cs.py
+++ scripts/python/cs.py
@@ -8,12 +8,48 @@ def main():
     """
     entry point for the program
     """
-    with open("/Users/kellsatnite/tools/scripts/rem.txt", "r", encoding="utf-8") as f:
-        lines = f.readlines()
 
-        for line in lines:
-            _line = line.strip()
-            print(_line)
+    rem_text = """
+    Keymaps
+        le -> go to end of the line
+        ls -> go to start of the line
+        <leader>gb -> git blame
+        <leader>td -> get the deleted lines in the current file
+        <leader>cc -> jump to current context
+        <leader>wK -> open witch key help
+        <leader>l -> lint file
+        <leader>ff -> Find files
+        <leader>fa -> Find all
+        <leader>fw -> Live grep
+        <leader>fb -> Find buffers
+        <leader>fh -> Help page
+        <leader>fo -> Find oldfiles
+        <leader>fz -> Find in current buffer
+
+        <leader>cm -> Git commits
+        <leader>gt -> Git status
+        <leader>th -> Nvchad themes
+
+        gd -> Go to definition
+        K -> hover documentation
+        gi -> Go to implementation
+        <leader>ra -> Rename symbol
+        <leader>ca -> Code action
+        <leader>/ -> comment toggle
+
+    For Folding
+
+        <leader>za -> Toggle fold under cursor
+        <leader>zc -> Toggle close fold under cursor
+        <leader>zC -> Toggle close on all folds under cursor
+        <leader>zM -> Toggle close all folds 
+        <leader>zo -> Toggle open fold under cursor
+        <leader>zO -> Toggle open on all folds under cursor
+        <leader>zR -> Toggle open on all folds
+
+    """
+
+    print(rem_text)
 
 
 if __name__ == "__main__":
