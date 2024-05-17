8176ae739c5b8bb5bcb8843757400455ab1a659f
Eshan Chatterjee <eshan2001221@gmail.com>
2024-05-17 20:03:06 +0530

    update setup scripts

--- run_stow.sh
+++ run_stow.sh
@@ -1,14 +1,31 @@
 #! /bin/zsh
 
+# How to stow.
+# while being in the dotfiles directory
+# stow -t <path_to_where_you_want_to_stow> <folder_to_stow>
+
+printf "Installing Rust\n"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+printf "Installing Homebrew\n"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 name=$(uname)
 
 if [[ $name == "Linux" ]]; then
+    sudo apt-get install build-essential procps curl file git cmake ninja jq ripgrep zsh
+    printf "Installing stow\n"
+    brew bundle install
     stow -d . -t /home/kellsatnite/ .
+    source ~/.zshrc
+    local WARP_ROOT=${XDG_DATA_HOME:-$HOME/.local/share}/warp-terminal
+    mkdir -p $WARP_ROOT
+    stow -t $WARP_ROOT .warp
+    printf "Warp terminal config was installed\n"
+    printf "Install Warp terminal manually\n"
 else
-   echo "message"
+    printf "Installing stow\n"
+    brew bundle install
     stow -d . -t /Users/kellsatnite/ .
+    source ~/.zshrc
 fi
-
-# to stow a perticular dir
-# stow -d <folder_path> -t <target_folder_path> things to stow from dir
