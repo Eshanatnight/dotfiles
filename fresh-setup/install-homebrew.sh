4a0c0bfbebe33c6901e6540e84a78edccfb97c7a
Eshan Chatterjee <eshan2001221@gmail.com>
2024-05-17 21:10:41 +0530

    cleanup

--- fresh-setup/install-homebrew.sh
+++
@@ -1,6 +0,0 @@
-cd ~;
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-test -d ~/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-test -r ~/.profile && echo "eval \"$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
-echo "eval \"$($(brew --prefix")/bin/brew shellenv)\"" >> ~/.profile
