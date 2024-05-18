e625e4256d67f2a0d255ea0e8ad2cbd70e3c63c1
Eshan Chatterjee <eshan2001221@gmail.com>
2024-05-19 02:29:42 +0530

    fix the setup pwsh script

--- setup.ps1
+++ setup.ps1
@@ -12,28 +12,34 @@ function mklink {
     New-Item -Path $link -ItemType SymbolicLink -Value $target
 }
 
+# Linking Config Files
 [string]$PWD_PATH = $PWD.Path
 [string]$GIT_CONFIG = "\.gitconfig"
-mklink($HOME + $GIT_CONFIG, $PWD + $GIT_CONFIG)
+mklink("$HOME$GIT_CONFIG".ToString()) ("$PWD_PATH$GIT_CONFIG".ToString())
+
+[string]$GIT_IGNORE = "\.ignore"
+mklink("$HOME$GIT_IGNORE".ToString()) ("$PWD_PATH$GIT_IGNORE".ToString())
 
 [string]$CARGO_CONFIG = "\.cargo\config.toml"
-mklink ($HOME + $CARGO_CONFIG) ($PWD_PATH + $CARGO_CONFIG)
+mklink ("$HOME$CARGO_CONFIG".ToString()) ("$PWD_PATH$CARGO_CONFIG".ToString())
 
 [string]$PROFILE_CONFIG = "\PowerShell\Microsoft.PowerShell_profile.ps1"
-mklink ($HOME + "\OneDrive\Documents" + $PROFILE_CONFIG) ($PWD_PATH + "\Terminal" + $PROFILE_CONFIG)
+mklink ("$HOME\OneDrive\Documents$PROFILE_CONFIG".ToString()) ("$PWD_PATH\Terminal$PROFILE_CONFIG".ToString())
 
 [string]$OH_MY_POSH_CONFIG = "\oh-my-posh\ohmyposhv3-v2.json"
-mklink ($HOME + "\Tweaks" + $OH_MY_POSH_CONFIG) ($PWD_PATH + "\Terminal\PowerShell\Theme" + $OH_MY_POSH_CONFIG)
+mklink ("$HOME\Tweaks$OH_MY_POSH_CONFIG".ToString()) ("$PWD_PATH\Terminal\PowerShell\Theme$OH_MY_POSH_CONFIG".ToString())
 
 [string]$WALLPAPERS = "\wallpapers"
-mklink ($HOME + "\OneDrive\" + $WALLPAPERS) ($PWD_PATH + "\wallpapers" + $WALLPAPERS)
+mklink ("$HOME\OneDrive\$WALLPAPERS".ToString()) ("$PWD_PATH\wallpapers$WALLPAPERS".ToString())
 
 mkdir $HOME + "\tools\scripts"
 Get-ChildItem -Path ($PWD_PATH + "\scripts\pwsh") -File | ForEach-Object {
     mklink ($HOME + "\tools\scripts\" + $_.Name) $_.FullName
 }
 
-[string[]]$PACKAGES = @("BurntSushi.ripgrep.MSVC","Bitwarden.Bitwarden", "ModernFlyouts.ModernFlyouts", "Microsoft.PowerToys", "JFrog.Conan", "Docker.DockerDesktop", "Git.Git", "Google.Chrome", "JesseDuffield.lazygit", "Ninja-build.Ninja", "JanDeDobbeleer.OhMyPosh", "Rainmeter.Rainmeter", "SumatraPDF.SumatraPDF", "VideoLAN.VLC", "ajeetdsouza.zoxide", "OliverSchwendener.ueli", "equalsraf.win32yank", "junegunn.fzf", "sharkdp.bat", "tldr-pages.tlrc", "Giorgiotani.Peazip", "Microsoft.PowerShell", "RevoUninstaller.RevoUninstaller", "Nilesoft.Shell", "GitHub.cli", "Kitware.CMake", "Microsoft.VisualStudioCode")
+
+Dependency Installation
+[string[]]$PACKAGES = @("BurntSushi.ripgrep.MSVC", "Bitwarden.Bitwarden", "ModernFlyouts.ModernFlyouts", "Microsoft.PowerToys", "JFrog.Conan", "Docker.DockerDesktop", "Git.Git", "Google.Chrome", "JesseDuffield.lazygit", "Ninja-build.Ninja", "JanDeDobbeleer.OhMyPosh", "Rainmeter.Rainmeter", "SumatraPDF.SumatraPDF", "VideoLAN.VLC", "ajeetdsouza.zoxide", "OliverSchwendener.ueli", "equalsraf.win32yank", "junegunn.fzf", "sharkdp.bat", "tldr-pages.tlrc", "Giorgiotani.Peazip", "Microsoft.PowerShell", "RevoUninstaller.RevoUninstaller", "Nilesoft.Shell", "GitHub.cli", "Kitware.CMake", "Microsoft.VisualStudioCode")
 
 foreach ($package in $PACKAGES) {
     winget install --id $package
@@ -41,7 +47,7 @@ foreach ($package in $PACKAGES) {
 
 Write-Output "Install Rustup and MSYS2 Manually"
 
-[string[]]$OPTIONAL_PACKAGES=@("FiloSottile.mkcert", "Postman.Postman", "AntibodySoftware.WizTree", "MiniTool.PartitionWizard.Free")
+[string[]]$OPTIONAL_PACKAGES = @("FiloSottile.mkcert", "Postman.Postman", "AntibodySoftware.WizTree", "MiniTool.PartitionWizard.Free")
 $INSTALL_OPTIONAL_PACKAGES = Read-Host "Do You Want to Install Optional Packages? (Y/N)"
 
 if ($INSTALL_OPTIONAL_PACKAGES -eq "Y") {
