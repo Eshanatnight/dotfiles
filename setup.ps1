6abe66e78a54ccf1411945fcafc529014317cecd
Eshan Chatterjee <eshan2001221@gmail.com>
2024-05-20 13:07:13 +0530

    chore: Update symbolic link creation logic in setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 # run winutils
-Invoke-RestMethod "https://christitus.com/win" | Invoke-Expression
+# Invoke-RestMethod "https://christitus.com/win" | Invoke-Expression
 
 function mklink {
     param(
@@ -8,40 +8,49 @@ function mklink {
         [Parameter(Mandatory = $true)]
         [string]$target
     )
-
-    New-Item -Path $link -ItemType SymbolicLink -Value $target
+    try {
+        if (Test-Path -Path $link) {
+            Write-Host "[INFO] Item already Exists $link, Bypassing Linking" -ForegroundColor Yellow
+            return
+        }
+        New-Item -Path $link -ItemType SymbolicLink -Value $target -ErrorAction Stop
+    } catch {
+        Write-Host "[ERROR] Failed to create symbolic link $link -> $target`n" -ForegroundColor Red
+        Write-Host $_.Exception.Message -ForegroundColor Red
+    }
 }
 
 # Linking Config Files
 [string]$PWD_PATH = $PWD.Path
 [string]$GIT_CONFIG = "\.gitconfig"
-mklink("$HOME$GIT_CONFIG".ToString()) ("$PWD_PATH$GIT_CONFIG".ToString())
+mklink -link "$HOME$GIT_CONFIG".ToString() -target "$PWD_PATH$GIT_CONFIG".ToString()
 
 [string]$GIT_IGNORE = "\.ignore"
-mklink("$HOME$GIT_IGNORE".ToString()) ("$PWD_PATH$GIT_IGNORE".ToString())
+mklink -link "$HOME$GIT_IGNORE".ToString() -target "$PWD_PATH$GIT_IGNORE".ToString()
 
 [string]$CARGO_CONFIG = "\.cargo\config.toml"
-mklink ("$HOME$CARGO_CONFIG".ToString()) ("$PWD_PATH$CARGO_CONFIG".ToString())
+mklink -link "$HOME$CARGO_CONFIG".ToString() -target "$PWD_PATH$CARGO_CONFIG".ToString()
 
 [string]$STARSHIP_CONFIG = "\.config\starship.toml"
-mklink("$HOME$STARSHIP_CONFIG".ToString()) ("$PWD_PATH$STARSHIP_CONFIG".ToString())
+mklink -link "$HOME$STARSHIP_CONFIG".ToString() -target "$PWD_PATH$STARSHIP_CONFIG".ToString()
 
 [string]$PROFILE_CONFIG = "\PowerShell\Microsoft.PowerShell_profile.ps1"
-mklink ("$HOME\OneDrive\Documents$PROFILE_CONFIG".ToString()) ("$PWD_PATH\Terminal$PROFILE_CONFIG".ToString())
+mklink -link "$HOME\OneDrive\Documents$PROFILE_CONFIG".ToString() -target "$PWD_PATH\Terminal$PROFILE_CONFIG".ToString()
 
 [string]$OH_MY_POSH_CONFIG = "\oh-my-posh\ohmyposhv3-v2.json"
-mklink ("$HOME\Tweaks$OH_MY_POSH_CONFIG".ToString()) ("$PWD_PATH\Terminal\PowerShell\Theme$OH_MY_POSH_CONFIG".ToString())
+mklink -link "$HOME\Tweaks$OH_MY_POSH_CONFIG".ToString() -target "$PWD_PATH\Terminal\PowerShell\Theme$OH_MY_POSH_CONFIG".ToString()
 
 [string]$WALLPAPERS = "\wallpapers"
-mklink ("$HOME\OneDrive\$WALLPAPERS".ToString()) ("$PWD_PATH\wallpapers$WALLPAPERS".ToString())
+mklink -link "$HOME\OneDrive$WALLPAPERS".ToString() -target "$PWD_PATH\wallpapers$WALLPAPERS".ToString()
 
-mkdir $HOME + "\tools\scripts"
+[string]$SCRIPTS_DIR="$HOME\tools\scripts".ToString()
+mkdir $SCRIPTS_DIR
 Get-ChildItem -Path ($PWD_PATH + "\scripts\pwsh") -File | ForEach-Object {
-    mklink ($HOME + "\tools\scripts\" + $_.Name) $_.FullName
+    mklink -link ($SCRIPTS_DIR + "\" + $_.Name) -target $_.FullName
 }
 
 
-Dependency Installation
+# Dependency Installation
 [string[]]$PACKAGES = @("BurntSushi.ripgrep.MSVC", "Bitwarden.Bitwarden", "ModernFlyouts.ModernFlyouts", "Microsoft.PowerToys", "JFrog.Conan", "Docker.DockerDesktop", "Git.Git", "Google.Chrome", "JesseDuffield.lazygit", "Ninja-build.Ninja", "JanDeDobbeleer.OhMyPosh", "Rainmeter.Rainmeter", "SumatraPDF.SumatraPDF", "VideoLAN.VLC", "ajeetdsouza.zoxide", "OliverSchwendener.ueli", "equalsraf.win32yank", "junegunn.fzf", "sharkdp.bat", "tldr-pages.tlrc", "Giorgiotani.Peazip", "Microsoft.PowerShell", "RevoUninstaller.RevoUninstaller", "Nilesoft.Shell", "GitHub.cli", "Kitware.CMake", "Microsoft.VisualStudioCode")
 
 foreach ($package in $PACKAGES) {
@@ -59,7 +68,11 @@ if ($INSTALL_OPTIONAL_PACKAGES -eq "Y") {
     }
 }
 
-if (!(Test-Path $HOME + "\tools\vcpkg")) {
-    Write-Output "Installing VCPkg"
-    git clone "https://github.com/microsoft/vcpkg.git" ($HOME + "\tools\vcpkg")
+
+if ($Error.Count -gt 0) {
+    Write-Host "Errors Occurred During Installation" -ForegroundColor Magenta
+    $response = Read-Host "Do You Want to View Errors? (Y/N)"
+    if ($? -and $response -eq "Y") {
+        Write-Host $Error -ForegroundColor DarkRed
+    }
 }
